0adc7a9f65c66a691c9ba37771da0dbd4d5bb615
sleepbot-zapp <neeladrijlt@gmail.com>
2024-08-02 20:10:23 +0530

    inital commit

---
+++ src/lib/tasks.ts
@@ -0,0 +1,6 @@
+export let tasks = [
+    {
+        title: "Heading",
+        desc: "Content"
+    }
+]
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+{#each tasks as task}
+    <div>
+        <h1 class="text-2xl font-bold underline">
+            {task.title}
+        </h1>
+    </div>
+{/each}
+  
+<style lang="postcss">
+</style>
+
+<script lang="ts">
+    import {tasks} from "../lib/tasks"
+</script>
